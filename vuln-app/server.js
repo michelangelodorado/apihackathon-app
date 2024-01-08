7af6803d65ec01a3a5efa58735b9eb06b82acd96
Mike <michelangelodorado@gmail.com>
2024-01-08 08:59:41 +0800

    Monday

--- vuln-app/server.js
+++ vuln-app/server.js
@@ -13,21 +13,26 @@ const PORT2 = 80;
 
 //DATABASE
 
-const ctflag1 = "WITCHERFLAG001";
-//const ctflag2 = "WITCHERFLAG002";
-const ctflag3 = "WITCHERFLAG003";
-const ctflag4 = "WITCHERFLAG004";
-const ctflag5 = "WITCHERFLAG005";
-const ctflag6 = "WITCHERFLAG006";
-const ctflag7 = "WITCHERFLAG007";
-const ctflag8 = "WITCHERFLAG008";
-const ctflag9 = "WITCHERFLAG009";
-const ctflag10 = "WITCHERFLAG010";
+const ctflag1 = "FLAG2267891044";
+//const ctflag2 = "FLAG3359748853"; check the JWT token
+const ctflag3 = "FLAG9628801771";
+const ctflag4 = "FLAG8721342307";
+const ctflag5 = "FLAG1465735767";
+const ctflag6 = "FLAG6313252739";
+const ctflag7 = "FLAG2276881316";
+const ctflag8 = "FLAG4030363181";
+const ctflag9 = "FLAG4975984665";
+const ctflag10 = "FLAG6963393046";
 const ctflag10_encoded = Buffer.from(ctflag10).toString('base64');
 
 const users = [
-  { userID: 'u001', username: 'maki', password: 'password1', firstname: "Maki", lastname: "Dee", phone: 94390842, birthday: "12/05/1993", email: 'Makidee@yahoo.com', secret: 'None', apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1pa2UiLCJyb2xlIjoiV0lUQ0hFUkZMQUcwMDJPTEQifQ._ureZEi4_uFHkDFUXONMHNSpEvgENye06yNHxyD4rWA' },
-  { userID: 'u002', username: 'davies', password: 'password2', firstname: "Davies", lastname: "Lee", phone: 83239651, birthday: "16/12/1987", email: 'Davieslee@gmail.com', secret: ctflag3,  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u001', username: 'maki', password: 'password1', firstname: "Maki", lastname: "Dee", phone: 94390842, birthday: "12/06/1994", email: 'makidee@yahoo.com', secret: 'None', apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1ha2kiLCJyb2xlIjoiRkxBRzMzNTk3NDg4NTMifQ.yK5CLmCBSVnRTEuC688fNVvjFr6Mqu_i_ybo0b2iNCY' },
+  { userID: 'u002', username: 'davies', password: 'nopass123@', firstname: "Davies", lastname: "Lee", phone: 83239651, birthday: "06/12/1987", email: 'davieslee@gmail.com', secret: ctflag3,  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u003', username: 'darryl', password: 'nopass123', firstname: "Darryl", lastname: "Loh", phone: 93239652, birthday: "12/13/1993", email: 'darryl.loh@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u004', username: 'frankie', password: 'nopass123', firstname: "Frank", lastname: "Cho", phone: 83139653, birthday: "12/15/1970", email: 'f.cho@rocketmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u005', username: 'julian', password: 'nopass123', firstname: "Joe", lastname: "Toro", phone: 86139758, birthday: "03/15/1971", email: 'joe_toro@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u006', username: 'wendell', password: 'nopass123', firstname: "Wendell", lastname: "Shane", phone: 43239757, birthday: "05/15/1990", email: 'w.shane@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u007', username: 'ky', password: 'nopass123', firstname: "ky", lastname: "cheo", phone: 43239757, birthday: "03/12/1969", email: 'kycheo@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
 ];
 
 const accountID = users.map(user => {
@@ -117,7 +122,7 @@ app.post('/login', (req, res, next) => {
   const isValidCredentials = checkCredentialsStuffing(username, password);
 
   if (isValidCredentials) {
-    return res.json({ challenge: ctflag10_encoded });
+    return res.json({ success: true, flag_encoded: ctflag10_encoded });
   }
 
   passport.authenticate('local', (err, user, info) => {
@@ -136,7 +141,7 @@ app.post('/login', (req, res, next) => {
 
 function checkCredentialsStuffing(username, password) {
   const Username = 'admin';
-  const Password = 'P@ssw0rd123';
+  const Password = 'P@ssw0rd';
   return username === Username && password === Password;
 }
 
@@ -147,7 +152,7 @@ app.get('/admin/v1', (req, res) => {
 app.get('/admin/hidden/v3',  (req, res) => {
   // Check if the 'apikey' header is present and has the correct value
   const apiKeyHeader = req.headers['apikey'];
-  const adminroleApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJhZG1pbiJ9.pIzs4EFu3LEBKfbE4wE6e0ysCeTh-nRUcpZNZ3caaY0";
+  const adminroleApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1ha2kiLCJyb2xlIjoiYWRtaW4ifQ.8OPLsjatk59WSBZk8tX07TZH2oxL4TO2rAWNdyG7wXk";
 
   if (apiKeyHeader === adminroleApiKey) {
     // If the header is valid, set the flag5 variable
@@ -155,7 +160,7 @@ app.get('/admin/hidden/v3',  (req, res) => {
     res.json({ flag: ctflag4 });
   } else {
     // If the header is not valid, return an error response
-    res.status(401).json({ error: 'Unauthorized' });
+    res.status(401).json({ error: 'Unauthorized, role not admin' });
   }
 });
 
@@ -224,7 +229,7 @@ const checkApiKeyNoValidation = (req, res, next) => {
 
 app.get('/cart.html', (req, res) => res.sendFile(path.join(__dirname, 'cart.html')));
 
-app.get('/api/user/profile/:userID', checkApiKey, (req, res) => {
+app.get('/api/v1/user/profile/:userID', checkApiKey, (req, res) => {
   const userId = req.params.userID;
   const userWithoutPassword = users.find(u => u.userID === userId);
 
@@ -255,11 +260,11 @@ app.post('/import', async (req, res) => {
   }
 });
 
-app.get('/api/products', checkApiKeyNoValidation, (req, res) => res.json(products));
+app.get('/api/v1/products', checkApiKeyNoValidation, (req, res) => res.json(products));
 
 app.get('/api/beta/products', (req, res) => res.json(betaproducts));
 
-app.put('/api/beta/products/:id', (req, res) => {
+app.get('/api/beta/products/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const { name, price, details } = req.body;
   const betaProduct = betaproducts.find(product => product.id === productId);
@@ -268,12 +273,31 @@ app.put('/api/beta/products/:id', (req, res) => {
     return res.status(404).json({ success: false, error: 'Product not found' });
   }
 
-  betaProduct.name = name || betaProduct.name;
-  betaProduct.price = price || betaProduct.price;
-  betaProduct.details = details || betaProduct.details;
-
-  res.json({ success: true, product: betaProduct, flag: `${ctflag8}` });
+  res.json({ success: true, product: betaProduct });
 });
+
+app.post('/api/beta/products/', (req, res) => {
+  const { id, name, price, details } = req.body;
+  const betaProduct = betaproducts.find(product => product.id === id);
+
+  if (!betaProduct) {
+    // If the product doesn't exist, create a new one
+    const newProduct = {
+      id,
+      name: name || '',
+      price: price || 0,
+      details: details || ''
+    };
+
+    betaproducts.push(newProduct);
+
+    res.json({ success: true, product: newProduct, flag: `${ctflag8}` });
+  } else {
+    return res.status(404).json({ success: false, error: 'Product ID exists' });
+  }
+});
+
+
 
 
 
@@ -282,6 +306,16 @@ app.put('/api/beta/products/:id', (req, res) => {
 app.post('/signup', (req, res) => {
 
   try {
+
+        // Check if the request has a body
+        if (!req.body) {
+          return res.status(400).json({ error: 'Request body is empty' });
+        }
+    
+        // Check if the body is valid JSON
+        if (Object.keys(req.body).length === 0 || !req.is('application/json')) {
+          return res.status(400).json({ error: 'Invalid JSON payload' });
+        }
   
     const { username, password, confirmPassword, email } = req.body;
   
