c628ee86352a2c5abe65cf968ffa58b8e3d25627
Mike <102953584+michelangelodorado@users.noreply.github.com>
2024-01-12 08:40:15 +0800

    Update server.js

--- vuln-app/server.js
+++ vuln-app/server.js
@@ -14,25 +14,25 @@ const PORT2 = 80;
 //DATABASE
 
 const ctflag1 = "FLAG2267891044";
-//const ctflag2 = "FLAG3359748853"; check the JWT token
-const ctflag3 = "FLAG9628801771";
-const ctflag4 = "FLAG8721342307";
-const ctflag5 = "FLAG1465735767";
-const ctflag6 = "FLAG6313252739";
-const ctflag7 = "FLAG2276881316";
-const ctflag8 = "FLAG4030363181";
+//const ctflag2 = "FLAG3359748853"; check the jwt
+const ctflag3 = "FLAG8721342307";
+const ctflag4 = "FLAG4030363181";
+const ctflag5 = "FLAG9628801771";
+const ctflag6 = "FLAG1465735767";
+const ctflag7 = "FLAG6313252739";
+const ctflag8 = "FLAG2276881316";
 const ctflag9 = "FLAG4975984665";
 const ctflag10 = "FLAG6963393046";
 const ctflag10_encoded = Buffer.from(ctflag10).toString('base64');
 
 const users = [
-  { userID: 'u001', username: 'maki', password: 'password1', firstname: "Maki", lastname: "Dee", phone: 94390842, birthday: "12/06/1994", email: 'makidee@yahoo.com', secret: 'None', apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1ha2kiLCJyb2xlIjoiRkxBRzMzNTk3NDg4NTMifQ.yK5CLmCBSVnRTEuC688fNVvjFr6Mqu_i_ybo0b2iNCY' },
-  { userID: 'u002', username: 'davies', password: 'nopass123@', firstname: "Davies", lastname: "Lee", phone: 83239651, birthday: "06/12/1987", email: 'davieslee@gmail.com', secret: ctflag3,  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
-  { userID: 'u003', username: 'darryl', password: 'nopass123', firstname: "Darryl", lastname: "Loh", phone: 93239652, birthday: "12/13/1993", email: 'darryl.loh@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
-  { userID: 'u004', username: 'frankie', password: 'nopass123', firstname: "Frank", lastname: "Cho", phone: 83139653, birthday: "12/15/1970", email: 'f.cho@rocketmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
-  { userID: 'u005', username: 'julian', password: 'nopass123', firstname: "Joe", lastname: "Toro", phone: 86139758, birthday: "03/15/1971", email: 'joe_toro@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
-  { userID: 'u006', username: 'wendell', password: 'nopass123', firstname: "Wendell", lastname: "Shane", phone: 43239757, birthday: "05/15/1990", email: 'w.shane@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
-  { userID: 'u007', username: 'ky', password: 'nopass123', firstname: "ky", lastname: "cheo", phone: 43239757, birthday: "03/12/1969", email: 'kycheo@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0123', username: 'maki', password: 'password1', firstname: "Maki", lastname: "Dee", phone: 94390842, birthday: "12/06/1994", email: 'makidee@yahoo.com', secret: 'None', apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1ha2kiLCJyb2xlIjoiRkxBRzMzNTk3NDg4NTMifQ.yK5CLmCBSVnRTEuC688fNVvjFr6Mqu_i_ybo0b2iNCY' },
+  { userID: 'u0224', username: 'davies', password: 'nopass123@', firstname: "Davies", lastname: "Lee", phone: 83239651, birthday: "06/12/1987", email: 'davieslee@gmail.com', secret: ctflag5,  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0352', username: 'darryl', password: 'nopass123', firstname: "Darryl", lastname: "Loh", phone: 93239652, birthday: "12/13/1993", email: 'darryl.loh@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0674', username: 'frankie', password: 'nopass123', firstname: "Frank", lastname: "Cho", phone: 83139653, birthday: "12/15/1970", email: 'f.cho@rocketmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0823', username: 'julian', password: 'nopass123', firstname: "Joe", lastname: "Toro", phone: 86139758, birthday: "03/15/1971", email: 'joe_toro@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0777', username: 'wendell', password: 'nopass123', firstname: "Wendell", lastname: "Shane", phone: 43239757, birthday: "05/15/1990", email: 'w.shane@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
+  { userID: 'u0125', username: 'ky', password: 'nopass123', firstname: "ky", lastname: "cheo", phone: 43239757, birthday: "03/12/1969", email: 'kycheo@gmail.com', secret: 'None',  apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAyIiwibmFtZSI6IkRhdiIsInJvbGUiOiJ1c2VyIn0.k-2jema74MuvZ1i8nbd3sJY00GRt_BaAg1MQpol2_IU' },
 ];
 
 const accountID = users.map(user => {
@@ -44,11 +44,9 @@ const accountID = users.map(user => {
 });
 
 const products = [
-  { id: 1, name: 'Elvis Live!', price: 350.00, location: 'Singapore Stadium', date: 'March 1, 2024', image: 'https://static.stalbert.ca/site/assets/files/30066/elvis_732_x_300_pixels_300dpi_-_st_albert.-full.jpg' },
-  { id: 2, name: 'Taylor Swift Tour', price: 400.00, location: 'Tokyo Dome', date: 'Feb 9, 2024', image: 'https://m.media-amazon.com/images/M/MV5BMmJlYWZmMzYtN2Y3OS00OTNkLTg2MzgtNjNkMTUyMzg0MTI3XkEyXkFqcGdeQXVyMzExODEzNDA@._V1_.jpg' },
+  { id: 1, name: 'Elvis Live!', price: 350, location: 'Singapore Stadium', date: 'March 1, 2024', image: 'https://static.stalbert.ca/site/assets/files/30066/elvis_732_x_300_pixels_300dpi_-_st_albert.-full.jpg' },
+  { id: 2, name: 'Taylor Swift Tour', price: 400, location: 'Tokyo Dome', date: 'Feb 9, 2024', image: 'https://m.media-amazon.com/images/M/MV5BMmJlYWZmMzYtN2Y3OS00OTNkLTg2MzgtNjNkMTUyMzg0MTI3XkEyXkFqcGdeQXVyMzExODEzNDA@._V1_.jpg' },
 ];
-
-const betaproducts = [{ id: 1, name: 'Beta Product 1', price: 25.00, details: 'This is a beta product!' }];
 
 // Passport configuration...
 // ...
@@ -132,26 +130,16 @@ function checkCredentialsStuffing(username, password) {
   return username === Username && password === Password;
 }
 
-//TESTING
-app.get('/mainnew', (req, res) => res.sendFile(path.join(__dirname, 'mainnew.html')));
-app.get('/addproduct', (req, res) => res.sendFile(path.join(__dirname, 'addproduct.html')));
-
-
-app.get('/admin/v1', (req, res) => {
+app.get('/admin/api', (req, res) => {
   res.json({ flag: `${ctflag1}` });
 });
-
-app.get('/admin/product/add',  (req, res) => {
-  // Check if the 'apikey' header is present and has the correct value
+app.get('/admin/api/add-product',  (req, res) => {
   const apiKeyHeader = req.headers['apikey'];
   const adminroleApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1MDAxIiwibmFtZSI6Ik1ha2kiLCJyb2xlIjoiYWRtaW4ifQ.8OPLsjatk59WSBZk8tX07TZH2oxL4TO2rAWNdyG7wXk";
 
   if (apiKeyHeader === adminroleApiKey) {
-    // If the header is valid, set the flag5 variable
-    //const flag5 = "Your desired value for flag5";
-    res.json({ flag: ctflag4 });
+    res.json({ flag: ctflag3 });
   } else {
-    // If the header is not valid, return an error response
     res.status(401).json({ error: 'Unauthorized, role not admin' });
   }
 });
@@ -183,11 +171,13 @@ app.get('/main.html', isAuthenticated, (req, res) => {
 
 });
 
-app.get('/accounts/list', (req, res) => {
-  res.json(accountID);
+app.get('/login', (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/main');
+  }
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/', (req, res) => res.redirect('/login'));
 
@@ -219,8 +209,6 @@ const checkApiKeyNoValidation = (req, res, next) => {
   }
 };
 
-app.get('/cart.html', (req, res) => res.sendFile(path.join(__dirname, 'cart.html')));
-
 app.get('/api/user/profile/:userID', checkApiKey, (req, res) => {
   const userId = req.params.userID;
   const userWithoutPassword = users.find(u => u.userID === userId);
@@ -233,17 +221,14 @@ app.get('/api/user/profile/:userID', checkApiKey, (req, res) => {
   }
 });
 
-app.get('/api/user/list', checkApiKey, (req, res) => {
-  res.json({ flag: `${ctflag8}`, accountID: accountID });
+app.get('/api/user/list', (req, res) => {
+  res.json({ flag: `${ctflag4}`, accountID: accountID });
 });
 
 app.post('/import', async (req, res) => {
   try {
     let { photoUrl } = req.body;
-
-    // Check if photoUrl starts with "http://" or "https://"
     if (!photoUrl.startsWith('http://') && !photoUrl.startsWith('https://')) {
-      // If not, prepend "http://"
       photoUrl = 'http://' + photoUrl;
     }
 
@@ -253,7 +238,7 @@ app.post('/import', async (req, res) => {
     const statusCode = response.status;
 
     if (statusCode == 200 && urlPort !== PORT) {
-      res.json({ photoUrl, statusCode, flag: `${ctflag7}` });
+      res.json({ photoUrl, statusCode, flag: `${ctflag8}` });
     } else {
       res.json({ photoUrl, statusCode });
     }
@@ -264,47 +249,7 @@ app.post('/import', async (req, res) => {
 });
 
 
-app.get('/api/v1/products', checkApiKeyNoValidation, (req, res) => res.json(products));
-
-app.get('/api/beta/products', (req, res) => res.json(betaproducts));
-
-app.get('/api/beta/products/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const { name, price, details } = req.body;
-  const betaProduct = betaproducts.find(product => product.id === productId);
-
-  if (!betaProduct) {
-    return res.status(404).json({ success: false, error: 'Product not found' });
-  }
-
-  res.json({ success: true, product: betaProduct });
-});
-
-app.post('/api/beta/products/', (req, res) => {
-  const { id, name, price, details } = req.body;
-  const betaProduct = betaproducts.find(product => product.id === id);
-
-  if (!betaProduct) {
-    // If the product doesn't exist, create a new one
-    const newProduct = {
-      id,
-      name: name || '',
-      price: price || 0,
-      details: details || ''
-    };
-
-    betaproducts.push(newProduct);
-
-    res.json({ success: true, product: newProduct, flag: `${ctflag8}` });
-  } else {
-    return res.status(404).json({ success: false, error: 'Product ID exists' });
-  }
-});
-
-
-
-
-
+app.get('/api/products', checkApiKeyNoValidation, (req, res) => res.json(products));
 
 // Handle sign-up request
 app.post('/signup', (req, res) => {
@@ -335,12 +280,11 @@ app.post('/signup', (req, res) => {
     }
     // Save user data (you might want to use a database for this)
     const newUser = { username, password, email };
-    //users.push(newUser);
-  
+
     return res.json({ message: 'User signed up successfully', user: newUser });
   } catch (error) {
       // In a vulnerable scenario, the error message may contain sensitive information
-      res.status(500).json({ flag: ctflag6, stackTrace: error.stack });
+      res.status(500).json({ flag: ctflag7, stackTrace: error.stack });
     }
   
   });
@@ -376,7 +320,7 @@ function handleBuyLogic(res, itemName, convertedPrice, quantity) {
       res.json({ success: true, convertedPrice });
     }
   } else if (quantity > 4) {
-    res.json({ success: true, flag: `${ctflag5}`, convertedPrice });
+    res.json({ success: true, flag: `${ctflag6}`, convertedPrice });
   } else {
     res.json({ success: false, error: 'Invalid quantity or item not found', convertedPrice });
   }
@@ -400,7 +344,7 @@ app.post('/buy', async (req, res) => {
     // Calculate the converted price
     const convertedPrice = calculateConvertedPrice(product, conversionRate);
 
-    console.log(`Product: ${itemName}, Original Price: ${product.price} USD, Conversion Rate: ${conversionRate}, Converted Price: ${convertedPrice} ${currencyCode}`);
+    //console.log(`Product: ${itemName}, Original Price: ${product.price} USD, Conversion Rate: ${conversionRate}, Converted Price: ${convertedPrice} ${currencyCode}`);
 
     // Handle buy logic
     handleBuyLogic(res, itemName, convertedPrice, quantity);
@@ -428,7 +372,7 @@ app.post('/convertcurrency', async (req, res) => {
     // Calculate the converted price
     const convertedPrice = calculateConvertedPrice(product, conversionRate);
 
-    console.log(`Product: ${productName}, Original Price: ${product.price} USD, Conversion Rate: ${conversionRate}, Converted Price: ${convertedPrice} ${currencyCode}`);
+    //console.log(`Product: ${productName}, Original Price: ${product.price} USD, Conversion Rate: ${conversionRate}, Converted Price: ${convertedPrice} ${currencyCode}`);
 
     // Return the product details and converted price to the frontend
     res.json({ product, convertedPrice });
